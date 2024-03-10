c1f0dc3c60670b838edbc94fedf9101f9890cb06
patrickluizdev <patrick@fasters.com.br>
2024-03-10 14:20:13 -0300

    Build: desativate addons StorybookConfig

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,6 +3,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
+    // Bug fixes github pages ↓ 
     // "@storybook/addon-links",
     // "@storybook/addon-essentials",
     // "@storybook/addon-onboarding",
