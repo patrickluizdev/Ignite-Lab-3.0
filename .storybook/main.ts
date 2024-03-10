4dd981b6aef0e6a8211fbc03acff58df482a9d06
patrickluizdev <patrick@fasters.com.br>
2024-03-10 17:21:59 -0300

    fit: Bug fixes github pages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,26 +4,19 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     // Bug fixes github pages ↓ 
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-    '@storybook/addon-a11y',
+    // "@storybook/addon-links",
+    // "@storybook/addon-essentials",
+    // "@storybook/addon-onboarding",
+    // "@storybook/addon-interactions",
+    // '@storybook/addon-a11y',
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
   docs: {
     autodocs: true,
-  },
-  features: {
-    "storyStoreV7": true,
   },
   viteFinal: (config, { configType }) => {
     if (configType == 'PRODUCTION'){
       config.base = '/ignite-lab-design-system/storybook-static/.'
-    }
+    }   
     return config
   }
 };
