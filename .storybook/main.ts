750fbdf67476530d06a3681263581df23ed00150
patrickluizdev <patrick@fasters.com.br>
2024-03-10 11:52:57 -0300

    chore: test config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -17,7 +17,7 @@ const config: StorybookConfig = {
   },
   viteFinal: (config, { configType }) => {
     if (configType == 'PRODUCTION'){
-      config.base = '/ignite-lab-design-system/'
+      config.base = '/ignite-lab-design-system/storybook-static/'
     }
     return config
   }
