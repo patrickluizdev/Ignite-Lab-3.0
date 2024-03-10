89891be94bca5a7f7f516e1c9264eb10210cccb8
patrickluizdev <patrick@fasters.com.br>
2024-03-10 14:52:19 -0300

    rm: StorybookConfig

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,7 +8,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     // "@storybook/addon-onboarding",
     // "@storybook/addon-interactions",
-    '@storybook/addon-a11y',
+    // '@storybook/addon-a11y',
   ],
   framework: {
     name: "@storybook/react-vite",
