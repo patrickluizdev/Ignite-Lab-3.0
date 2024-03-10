00d2026608f0e6e58315a7efd8c527bfb5757761
patrickluizdev <patrick@fasters.com.br>
2024-03-09 22:56:33 -0300

    Refactor: add color as object

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,16 +15,17 @@ export default {
     colors:{
       'black': '#000',
       'white': '#FFF',
-
-      'gray-900': '#121214',
-      'gray-800': '#202024',
-      'gray-400': '#7C7C8A',
-      'gray-200': '#C4C4CC',
-      'gray-100': '#E1E1E6',
-
-      'cyan-500': '#81D8F7',
-      'cyan-200': '#A1E1F9',
-
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7C7C8A',
+        200: '#C4C4CC',
+        100: '#E1E1E6'
+      },
+      cyan: {
+        500: '#81D8F7',
+        200: '#A1E1F9'
+      }
     },
     extend: {
       fontFamily:{
