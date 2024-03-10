7c1bfb731f07e84f566c4b8bb9bc738b135b768f
patrickluizdev <patrick@fasters.com.br>
2024-03-10 14:14:18 -0300

    ci: test config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,10 +3,10 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
+    // "@storybook/addon-links",
+    // "@storybook/addon-essentials",
     // "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
+    // "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-vite",
