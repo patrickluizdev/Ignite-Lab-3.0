65602effb8df1ba8ac2ce2d1d8d6066e336662e6
patrickluizdev <patrick@fasters.com.br>
2024-03-10 17:35:10 -0300

    fix: add storybook addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,11 +4,11 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     // Bug fixes github pages ↓ 
-    // "@storybook/addon-links",
-    // "@storybook/addon-essentials",
-    // "@storybook/addon-onboarding",
-    // "@storybook/addon-interactions",
-    // '@storybook/addon-a11y',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: "@storybook/react-vite",
