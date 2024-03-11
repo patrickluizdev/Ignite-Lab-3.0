35fdaac46c23456e2a825976a5335cd71a5103c0
patrickluizdev <patrick@fasters.com.br>
2024-03-10 23:51:56 -0300

    ci: config baseUrl and outDir vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig, InlineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ignite-lab-design-system/application-static/.',
   build: {
     outDir: 'static/application-static/.'
   }
